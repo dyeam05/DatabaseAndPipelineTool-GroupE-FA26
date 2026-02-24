20907a3b2e15cc89ed91b70411604c913b75657b
noahapursell <noahapursell@gmail.com>
2026-02-24 09:37:26 -0600

    updated setup to init conda env and removed uneccsary commands

--- scripts/setup-openpilot.sh
+++ scripts/setup-openpilot.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-set -euo pipefail # this makes the script stop if something fails
+set -euo pipefail
 
-# make sure the user passed in an arguement for the conda env name
 if [ "$#" -lt 1 ]; then
 	echo "Usage: $0 conda_env_name"
 	exit 1
@@ -12,21 +11,15 @@ echo "Will use ($1) as environment name"
 conda_env_name="$1"
 
 
-# clone openpilot
 git clone --filter=blob:none --recurse-submodules --also-filter-submodules https://github.com/commaai/openpilot.git
 cd openpilot
-# checkout v0.9.8
 git checkout v0.9.8
 git lfs pull
-# create conda env - this way we know our python  will be 3.12
 conda create -n "$conda_env_name" python=3.12 -y
+source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate "$conda_env_name"
-# refresh this because some of the hashes in the uv lock are old
+# tools/op.sh setup # this errors due to a hash mismatch
 uv lock --refresh
-# Run the dependency installer
 tools/op.sh setup # now it works
-# Activate the venv python env
 source .venv/bin/activate
-# Build the project
 scons -u -j$(nproc)
-
