792b99bb88e973d5f9bb858db08bb3e3585bfaf7
noahapursell <noahapursell@gmail.com>
2026-02-27 09:59:55 -0600

    added script to register jwt

--- scripts/setup-openpilot.sh
+++ scripts/setup-openpilot.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 set -x
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en
+export LC_ALL=en_US.UTF-8
+export NVIDIA_VISIBLE_DEVICES=all
+export NVIDIA_DRIVER_CAPABILITIES=graphics,utility,compute
+export DEBIAN_FRONTEND=noninteractive
 
 if [ "$#" -lt 3 ]; then
 	echo "Usage: $0 <conda_env_name> <run_model_on_cpu_or_not (true or false)> <replace_cereal_service_with_gps_compatible_ceral_serivice_or_not (true or false)>"
@@ -13,12 +19,27 @@ conda_env_name="$1"
 run_model_on_cpu="$2"
 replace_cereal_with_gps_cereal="$3"
 
+# Install packages
+apt-get update
+apt-get install -y --no-install-recommends sudo tzdata locales wget file git git-lfs
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen
+
 
 if [ "$run_model_on_cpu" = "true" ]; then
 	sudo apt update
 	sudo apt install -y pocl-opencl-icd
 fi
 
+
+# Install conda
+cd ~
+apt-get install -y ca-certificates
+update-ca-certificates
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ~/Miniconda3-latest-Linux-x86_64.sh -b
+source ~/miniconda3/etc/profile.d/conda.sh
+conda tos accept
+cd /workspace
 
 git clone --filter=blob:none --recurse-submodules --also-filter-submodules https://github.com/commaai/openpilot.git
 cd openpilot
